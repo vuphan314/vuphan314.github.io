702fe41da8b7189ba2e438e2b49d79cd74d50104
Vu <vuphan314@gmail.com>
2017-06-21 22:21:42 -0500

    main.sh

---
+++ main.sh
@@ -0,0 +1,8 @@
+# docs
+cp ../docs/photo.jpg docs/
+cp ../docs/*.pdf docs/
+# papers
+cp ~/repos/crprolog/paper/paper.pdf papers/crProlog.pdf
+cp ~/Dropbox/debt/comparability/paper/paper.pdf papers/comparability.pdf
+# transcript
+cp ~/Dropbox/other/Vu/transcripts/TTU_unofficial.pdf transcript.pdf
