7dff615207b2d32c155dcdd3c630fed76e86d4d6
Vu Phan <vuphan314@gmail.com>
2017-06-27 11:48:12 -0500

    main.bat

---
+++ main.bat
@@ -0,0 +1,8 @@
+set cmd_copy=xcopy /y
+cls
+
+::papers
+%cmd_copy% D:\repos\crprolog\paper\paper.pdf papers\crProlog.pdf
+%cmd_copy% D:\Dropbox\debt\comparability\paper\paper.pdf papers\comparability.pdf
+::transcript
+%cmd_copy% D:\Dropbox\other\Vu\transcripts\TTU_unofficial.pdf transcript.pdf
