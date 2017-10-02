527f89d93e9f37a08352d1e042a6ed53f874b1d1
Vu <vuphan314@gmail.com>
2017-10-01 21:12:59 -0500

    bash doc comment starts with ##

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
-# papers
-cp ~/repos/crprolog/paper/paper.pdf papers/crProlog.pdf
+## papers
+# cp ~/repos/crprolog/paper/paper.pdf papers/crProlog.pdf
 # cp ~/repos/comparability/comparability.pdf papers/comparability.pdf
-# transcript
+## transcript
 cp ~/Dropbox/other/Vu/transcripts/TTU_unofficial.pdf transcript.pdf
