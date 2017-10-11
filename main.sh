82c994d60dcc1bf9499e8bc1ce20cba2ce6d4c29
Vu <vuphan314@gmail.com>
2017-10-11 11:28:22 -0500

    ~/github/

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 ## papers
-# cp ~/repos/crprolog/paper/paper.pdf papers/crProlog.pdf
-# cp ~/repos/comparability/comparability.pdf papers/comparability.pdf
+# cp ~/github/crprolog/paper/paper.pdf papers/crProlog.pdf
+# cp ~/github/comparability/comparability.pdf papers/comparability.pdf
 ## transcript
 cp ~/Dropbox/other/Vu/transcripts/TTU_unofficial.pdf transcript.pdf
