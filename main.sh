1f504d0731523967a130b24012d709a723675c89
Vu Phan <vuphan314@users.noreply.github.com>
2019-01-05 16:48:54 -0600

    updated homepage

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 ## papers
-# cp ~/github/crprolog/paper/paper.pdf papers/crprolog.pdf
-cp ~/github/comparability/comparability.pdf papers/comparability.pdf
+# cp ~/code/crprolog/paper/paper.pdf papers/crprolog.pdf
+# cp ~/code/comparability/comparability.pdf papers/comparability.pdf
 
 ## transcript
-cp ~/Dropbox/other/Vu/transcripts/TTU_unofficial.pdf transcript.pdf
+# cp ~/Dropbox/other/Vu/transcripts/TTU_unofficial.pdf transcript.pdf
