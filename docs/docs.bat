6c5ce4f8cb29c6b651934a74255d8b7a9f0aac9b
Vu Phan <vuphan314@users.noreply.github.com>
2020-09-04 22:33:00 -0500

    cvVuPhan.*

--- docs/docs.bat
+++
@@ -1,4 +0,0 @@
-REM latexmk -C
-REM latexmk -pdf -synctex=1 cv.tex
-REM latexmk -pdf -synctex=1 sop.tex
-latexmk -pdf -synctex=1 sor.tex
--- docs/docs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-################################################################################
-
-# latexmk -C
-latexmk -pdf -synctex=1 cv.tex
-latexmk -pdf -synctex=1 sop.tex
-latexmk -pdf -synctex=1 sor.tex
